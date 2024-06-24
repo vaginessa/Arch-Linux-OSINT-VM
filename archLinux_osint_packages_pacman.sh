1e9df3e52d33507da514832e69b17d519f389281
Zombi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-06-24 16:20:22 -0700

    Update archLinux_osint_packages_pacman.sh

--- archLinux_osint_packages_pacman.sh
+++ archLinux_osint_packages_pacman.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo | pacman -S keepassxc wireshark-qt aircrack-ng
-echo | pacman -S gospider hakrawler ffuf seclists filezilla 
-echo | pacman -S python-virtualenv python-pipx python-pip
-echo | pacman -S httrack sherlock eyewitness sublist3r photon recon-ng
-echo | pacman -S go postgresql ngrep
-echo | pacman -S nmap tcpdump php audacity gimp
-echo | pacman -S librecad libreoffice
-echo | pacman -S cherrytree ripgrep wordlister crunch hashcat
-echo | pacman -S dbeaver proxychains tor
-echo | pacman -S vlc mpv mat2 sequoia-sq
-echo | pacman -S marble qgis finalrecon ugrep bloodhound bing-ip2hosts
-echo | pacman -S thunderbird exiflooter flowblade
-echo | pacman -S ngrok telegram-desktop pidgin gajim
-echo | pacman -S metasploit maltego gophish
+pacman -S keepassxc wireshark-qt aircrack-ng
+pacman -S gospider hakrawler ffuf seclists filezilla 
+pacman -S python-virtualenv python-pipx python-pip
+pacman -S httrack sherlock eyewitness sublist3r photon recon-ng
+pacman -S go postgresql ngrep
+pacman -S nmap tcpdump php audacity gimp
+pacman -S librecad libreoffice
+pacman -S cherrytree ripgrep wordlister crunch hashcat
+pacman -S dbeaver proxychains tor
+pacman -S vlc mpv mat2 sequoia-sq
+pacman -S marble qgis finalrecon ugrep bloodhound bing-ip2hosts
+pacman -S thunderbird exiflooter flowblade
+pacman -S ngrok telegram-desktop pidgin gajim
+pacman -S metasploit maltego gophish
 pacman -S blackarch-stego
-echo | pacman -S wpscan evil-winrm
-echo | pacman -S burpsuite
+pacman -S wpscan evil-winrm
+pacman -S burpsuite

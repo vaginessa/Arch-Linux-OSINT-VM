4a81b9337f681caba470e8a934672bd7397d5683
Zombi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-08-09 15:50:39 -0700

    Update archLinux_osint_packages_pacman.sh

--- archLinux_osint_packages_pacman.sh
+++ archLinux_osint_packages_pacman.sh
@@ -9,7 +9,7 @@ pacman -S nmap tcpdump php audacity gimp
 pacman -S librecad libreoffice mimikatz
 pacman -S cherrytree ripgrep wordlister crunch hashcat
 pacman -S dbeaver proxychains tor powersploit
-pacman -S vlc mpv mat2 sequoia-sq
+pacman -S vlc mpv mat2 sequoia-sq npm
 pacman -S marble qgis finalrecon ugrep bloodhound bing-ip2hosts
 pacman -S thunderbird exiflooter flowblade
 pacman -S ngrok telegram-desktop pidgin gajim
@@ -24,3 +24,5 @@ pacman -S reaver bully cowpatty pyrit macchanger hcxdumptool hcxtools john
 pacman -S mongodb nosqlattack nosqli-user-pass-enum remmina
 systemctl start sshd
 systemctl enable sshd
+npm install -g redis-cli
+npm install -g ftp-spider

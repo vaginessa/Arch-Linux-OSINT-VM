36e9172c3d975e034d51b1289c6e743f6e522f75
Zombi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-08-10 09:28:09 -0700

    Update archLinux_osint_packages_pacman.sh

--- archLinux_osint_packages_pacman.sh
+++ archLinux_osint_packages_pacman.sh
@@ -21,7 +21,7 @@ pacman -S kismet bettercap routersploit
 pacman -S burpsuite
 pacman -S sn0int sqlitebrowser hydra wifite
 pacman -S reaver bully cowpatty pyrit macchanger hcxdumptool hcxtools john
-pacman -S mongodb nosqlattack nosqli-user-pass-enum remmina smbclient nosqlmap
+pacman -S mongodb nosqlattack nosqli-user-pass-enum remmina smbclient nosqlmap mongoaudit
 systemctl start sshd
 systemctl enable sshd
 npm install -g redis-cli
